91af6ce42f8057e1b5aacbf0fe9891db37716955
Matt Cowan <mcowan@atlassian.com>
2019-12-30 14:48:36 -0500

    Remove async keyword

--- scripts/connector.js
+++ scripts/connector.js
@@ -1,10 +1,11 @@
 /* global TrelloPowerUp */
 
 TrelloPowerUp.initialize({
-  'card-back-section': async t => {
+  'card-back-section': t => {
     return {
       title: 'My Card Back Section',
-      icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg',
+      icon:
+        'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg',
       content: {
         type: 'iframe',
         url: t.signUrl('./snippet.html'),
