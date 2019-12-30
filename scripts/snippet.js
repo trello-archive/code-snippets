f2206ff755b9cb0dd8f4e3227d4e29f1c62fbec3
Matt Cowan <mcowan@atlassian.com>
2019-12-30 15:52:49 -0500

    Setup monaco environments

--- scripts/snippet.js
+++ scripts/snippet.js
@@ -1,5 +1,23 @@
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.main';
 
+self.MonacoEnvironment = {
+  getWorkerUrl(moduleId, label) {
+    if (label === 'json') {
+      return './json.worker.js';
+    }
+    if (label === 'css') {
+      return './css.worker.js';
+    }
+    if (label === 'html') {
+      return './html.worker.js';
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return './ts.worker.js';
+    }
+    return './editor.worker.js';
+  },
+};
+
 monaco.editor.create(document.getElementById('container'), {
   value: `function x() {\n\tconsole.log("Hello world!");\n}`,
   language: 'javascript',
